89bf94db038e47e9fedfb062c7c66480c4305e15
Andre Segura <andrensegura@gmail.com>
2018-04-24 23:36:11 -0700

    base entity class added

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ pygame.init()
 fpsClock = pygame.time.Clock()
 
 # window surface object
-windowSurface = pygame.display.set_mode((400,300))
+WIN_WIDTH  = 480
+WIN_HEIGHT = 300
+windowSurface = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Jumpy")
 
 # colors
@@ -16,7 +18,23 @@ backgr = pygame.Color(110,110,110)
 black  = pygame.Color(0,0,0)
 white  = pygame.Color(255, 255, 255)
 
+#entities
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, color, x, y):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([WIN_WIDTH / 15, WIN_WIDTH / 15])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 # game loop
+
+entities = pygame.sprite.Group()
+entities.add(Entity(blue, 100, 100))
+
 while True:
     windowSurface.fill(backgr)
 
@@ -25,5 +43,6 @@ while True:
             pygame.quit()
             sys.exit()
     
+    entities.draw(windowSurface)
     pygame.display.update()
     fpsClock.tick(30)
